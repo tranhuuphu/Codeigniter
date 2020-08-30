09c9cc081f8b8084fa75c0bf840ba676e370469a
tranhuuphu <tranhuuphu9x@gmail.com>
2020-08-30 21:55:03 +0700

    add

--- app/Views/admin/header.php
+++ app/Views/admin/header.php
@@ -8,20 +8,11 @@
   <link rel="stylesheet" href="<?= base_url('/'); ?>/public/AdminLTE/plugins/fontawesome-free/css/all.min.css">
   <!-- Ionicons -->
   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-  <!-- Tempusdominus Bbootstrap 4 -->
-  <!-- <link rel="stylesheet" href="<?= base_url('/'); ?>/public/AdminLTE/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css"> -->
-  <!-- iCheck -->
-  <!-- <link rel="stylesheet" href="<?= base_url('/'); ?>/public/AdminLTE/plugins/icheck-bootstrap/icheck-bootstrap.min.css"> -->
-  <!-- JQVMap -->
-  <!-- <link rel="stylesheet" href="<?= base_url('/'); ?>/public/AdminLTE/plugins/jqvmap/jqvmap.min.css"> -->
-  <!-- Theme style -->
+
   <link rel="stylesheet" href="<?= base_url('/'); ?>/public/AdminLTE/dist/css/adminlte.min.css">
   <!-- overlayScrollbars -->
   <link rel="stylesheet" href="<?= base_url('/'); ?>/public/AdminLTE/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
-  <!-- Daterange picker -->
-  <!-- <link rel="stylesheet" href="<?= base_url('/'); ?>/public/AdminLTE/plugins/daterangepicker/daterangepicker.css"> -->
-  <!-- summernote -->
-  <!-- <link rel="stylesheet" href="<?= base_url('/'); ?>/public/AdminLTE/plugins/summernote/summernote-bs4.css"> -->
+
 
   <link rel="stylesheet" href="<?= base_url('/'); ?>/public/AdminLTE/plugins/tag-input/amsify.suggestags.css">
   <!-- Google Font: Source Sans Pro -->
